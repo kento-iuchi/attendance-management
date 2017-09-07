6c9ffc93fa372cc5548582b501c604a1346bcd15
kento-iuchi <kento.iuchi@e-grant.net>
2017-09-08 03:22:36 +0900

    リモートからconfig.phpを削除

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-ini_set('display_errors', 1);
-
-define('DSN', 'mysql:host=localhost;dbname=attendance_management');
-define('DB_USERNAME', 'develop_user');
-define('DB_PASSWORD', 'develop');
